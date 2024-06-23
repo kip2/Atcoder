a611466270321d66356850d985d30cb4a71ec95b
kip2 <tqa1no4@gmail.com>
2024-06-23 21:22:52 +0900

    Clear a02

--- tessoku-book/src/bin/a02.rs
+++ tessoku-book/src/bin/a02.rs
@@ -1,3 +1,17 @@
 fn main() {
-    todo!();
+    proconio::input! {
+        N: usize,
+        X: i32,
+        A: [i32; N],
+    }
+
+    let mut flg = "No";
+
+    for i in 0..N {
+        if A[i] == X {
+            flg = "Yes";
+        }
+    }
+
+    println!("{}", flg);
 }
