7050b0e178c49ffd10e02bf2e236d6bcabce8232
kip2 <tqa1no4@gmail.com>
2024-06-25 20:29:17 +0900

    Tessoku-book: Clear a05

--- tessoku-book/src/bin/a05.rs
+++ tessoku-book/src/bin/a05.rs
@@ -1,3 +1,19 @@
 fn main() {
-    todo!();
+    proconio::input! {
+        n: i32,
+        k: i32,
+    }
+
+    let mut cnt = 0;
+
+    for i in 1..=n {
+        for j in 1..=n {
+            let buf = k - (i + j);
+            if 1 <= buf && buf <= n {
+                cnt += 1;
+            }
+        }
+    }
+
+    println!("{}", cnt);
 }
