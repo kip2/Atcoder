f2c59ed42dae2aa7d92fbcd0066a2fba9ddd4eba
kip2 <tqa1no4@gmail.com>
2024-06-29 08:59:14 +0900

    Tessoku-book Clear a07

--- tessoku-book/src/bin/a07.rs
+++ tessoku-book/src/bin/a07.rs
@@ -1,3 +1,34 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        d: usize,
+        n: usize,
+    }
+
+    let mut lr = vec![];
+    for _ in 0..n {
+        input! {
+            l: usize,
+            r: usize,
+        }
+        lr.push((l, r));
+    }
+
+    let mut arr = vec![0; d + 2];
+
+    for (l, r) in lr {
+        arr[l] += 1;
+        arr[r + 1] -= 1;
+    }
+
+    let mut answer = vec![0; d + 1];
+
+    for i in 1..=d {
+        answer[i] = answer[i - 1] + arr[i];
+    }
+
+    for i in 1..=d {
+        println!("{}", answer[i]);
+    }
 }
