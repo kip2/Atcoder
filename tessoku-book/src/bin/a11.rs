a953f72bc62229660330d54aca5f0c4dd5929aeb
kip2 <tqa1no4@gmail.com>
2024-07-02 20:17:26 +0900

    Tessoku-book: Clear a11

--- tessoku-book/src/bin/a11.rs
+++ tessoku-book/src/bin/a11.rs
@@ -1,3 +1,31 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        x: usize,
+        a: [usize; n],
+    }
+
+    let mut a_with_zero = vec![0];
+    a_with_zero.extend(a);
+
+    println!("{}", search(x, n, &a_with_zero));
+}
+
+fn search(x: usize, n: usize, a: &Vec<usize>) -> i32 {
+    let mut l = 0;
+    let mut r = n;
+
+    while l <= r {
+        let m = (l + r) / 2;
+        if x < a[m] {
+            r = m - 1
+        } else if x > a[m] {
+            l = m + 1
+        } else if x == a[m] {
+            return m as i32;
+        }
+    }
+    -1
 }
