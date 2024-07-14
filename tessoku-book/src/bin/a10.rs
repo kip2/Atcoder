bd7519f4c552f3cddb188f959d496497b57f791a
kip2 <tqa1no4@gmail.com>
2024-07-14 13:50:08 +0900

    Tessoku-book: Clear a10

--- tessoku-book/src/bin/a10.rs
+++ tessoku-book/src/bin/a10.rs
@@ -1,3 +1,32 @@
+use proconio::input;
+use std::cmp::max;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a_buf: [usize; n],
+        d: usize,
+    }
+
+    let mut a = vec![0];
+    a.extend(a_buf);
+
+    let mut left_c_sum: Vec<usize> = vec![0; n + 2];
+    let mut right_c_sum: Vec<usize> = vec![0; n + 2];
+
+    for i in 1..=n {
+        left_c_sum[i] = max(a[i], left_c_sum[i - 1]);
+    }
+    for i in (1..=n).rev() {
+        right_c_sum[i] = max(a[i], right_c_sum[i + 1]);
+    }
+
+    for i in 0..d {
+        input! {
+            l:usize,
+            r:usize,
+        }
+
+        println!("{}", max(left_c_sum[l - 1], right_c_sum[r + 1]));
+    }
 }
