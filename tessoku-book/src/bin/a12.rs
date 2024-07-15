d9b7eb12bd5c4c80ec1e401203574164dc6698a1
kip2 <tqa1no4@gmail.com>
2024-07-15 13:50:48 +0900

    Tessoku-book: Clear a12

--- tessoku-book/src/bin/a12.rs
+++ tessoku-book/src/bin/a12.rs
@@ -1,3 +1,38 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        k: usize,
+        a_buf: [usize; n],
+    }
+
+    let mut a = vec![0];
+    a.extend(a_buf);
+
+    let mut l = 1;
+    let mut r = 1000000000;
+
+    while l < r {
+        let mid = (l + r) / 2;
+        if check(mid, k, n, &a) {
+            r = mid;
+        } else {
+            l = mid + 1;
+        }
+    }
+
+    println!("{}", l);
+}
+
+fn check(x: usize, k: usize, n: usize, a: &[usize]) -> bool {
+    let mut sum = 0;
+    for i in 1..=n {
+        sum += x / a[i];
+    }
+    if sum >= k {
+        true
+    } else {
+        false
+    }
 }
