14b4b8be9d31eeb1a6f89f55c5d4748d8fc3e131
kip2 <tqa1no4@gmail.com>
2024-07-15 16:42:55 +0900

    ABC191: Clear b

--- abc191/src/bin/b.rs
+++ abc191/src/bin/b.rs
@@ -1,3 +1,27 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        x: usize,
+        a: [usize; n],
+    }
+
+    let mut answer: Vec<usize> = vec![];
+
+    for i in 0..n {
+        if a[i] != x {
+            answer.push(a[i]);
+        }
+    }
+
+    if answer.len() != 0 {
+        let output = answer
+            .iter()
+            .map(|x| x.to_string())
+            .collect::<Vec<String>>()
+            .join(" ");
+
+        println!("{}", output);
+    }
 }
