0146e7819963c5c03b72a4da2d4ed36534737544
kip2 <tqa1no4@gmail.com>
2024-07-15 16:14:02 +0900

    ABC197: Clear a

--- abc197/src/bin/a.rs
+++ abc197/src/bin/a.rs
@@ -1,3 +1,12 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        s: String,
+    }
+
+    let first_char = &s[0..1];
+    let rest = &s[1..];
+
+    println!("{}", format!("{}{}", rest, first_char));
 }
