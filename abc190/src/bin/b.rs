c42d0a5680e78a8de17385e9f7584381fa1b2f70
kip2 <tqa1no4@gmail.com>
2024-07-15 16:56:25 +0900

    ABC190: Clear b

--- abc190/src/bin/b.rs
+++ abc190/src/bin/b.rs
@@ -1,3 +1,23 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        s: usize,
+        d: usize,
+    }
+
+    for i in 0..n {
+        input! {
+            x: usize,
+            y: usize,
+        }
+
+        if x < s && d < y {
+            println!("Yes");
+            return;
+        }
+    }
+
+    println!("No");
 }
