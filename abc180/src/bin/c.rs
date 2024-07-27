fcd41c492eb25c3256203b6dab9fe4cd728164f0
kip2 <tqa1no4@gmail.com>
2024-07-27 14:00:44 +0900

    Clear ABC180 C

--- abc180/src/bin/c.rs
+++ abc180/src/bin/c.rs
@@ -1,3 +1,23 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+    }
+
+    let mut answer = vec![];
+
+    for i in 1..=(n as f64).sqrt() as usize {
+        if n % i == 0 {
+            answer.push(i);
+            if i != n / i {
+                answer.push(n / i);
+            }
+        }
+    }
+    answer.sort();
+
+    for i in 0..answer.len() {
+        println!("{}", answer[i]);
+    }
 }
