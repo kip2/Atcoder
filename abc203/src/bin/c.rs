df2044cd8b0b1caa64d11545625550d69c20d771
kip2 <tqa1no4@gmail.com>
2024-07-28 13:55:19 +0900

    Clear ABC203 C

--- abc203/src/bin/c.rs
+++ abc203/src/bin/c.rs
@@ -1,3 +1,35 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        k: usize,
+    }
+
+    let mut friends: Vec<(usize, usize)> = vec![];
+
+    for _ in 0..n {
+        input! {
+            a: usize,
+            b: usize,
+        }
+        friends.push((a, b));
+    }
+
+    friends.sort();
+
+    let mut now_v = k;
+
+    for i in 0..n {
+        let f_v = friends[i].0;
+        let f_m = friends[i].1;
+
+        if f_v <= now_v {
+            now_v += f_m;
+        } else {
+            break;
+        }
+    }
+
+    println!("{}", now_v);
 }
