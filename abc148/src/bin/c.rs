619cc82cc9d0ab42595603aa4c7fb4630d2f5dd0
kip2 <tqa1no4@gmail.com>
2024-08-03 13:32:36 +0900

    ABC148: Clear C

--- abc148/src/bin/c.rs
+++ abc148/src/bin/c.rs
@@ -1,3 +1,12 @@
+use num::integer;
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        a: usize,
+        b: usize,
+    }
+
+    let answer = integer::lcm(a, b);
+    println!("{}", answer);
 }
