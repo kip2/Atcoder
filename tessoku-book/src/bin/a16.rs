4a703a0a2344be4112163a1ac72fc77ad90d53b3
kip2 <tqa1no4@gmail.com>
2024-08-04 13:55:17 +0900

    Tessoku-book: Clear a16

--- tessoku-book/src/bin/a16.rs
+++ tessoku-book/src/bin/a16.rs
@@ -1,3 +1,31 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a_buf: [usize; n-1],
+        b_buf: [usize; n-2],
+    }
+
+    let mut a = vec![0];
+    let mut b = vec![0, 0];
+
+    a.extend(a_buf);
+    b.extend(b_buf);
+
+    let mut dp = Vec::new();
+    dp.push(0);
+
+    for i in 0..n {
+        if i == 0 {
+            continue;
+        } else if i == 1 {
+            dp.push(a[i]);
+            continue;
+        }
+
+        dp.push(std::cmp::min(a[i] + dp[i - 1], b[i] + dp[i - 2]));
+    }
+
+    println!("{}", dp[n - 1]);
 }
