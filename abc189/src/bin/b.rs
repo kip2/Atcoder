65d51f6a4faa6799d80763bc55b287eee46f7904
kip2 <tqa1no4@gmail.com>
2024-08-04 13:24:32 +0900

    ABC189: Clear B

--- abc189/src/bin/b.rs
+++ abc189/src/bin/b.rs
@@ -1,3 +1,25 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        x: i64,
+    }
+
+    let mut answer: i64 = -1;
+    let mut alcohol = 0;
+
+    for i in 0..n {
+        input! {
+            v: i64,
+            p: i64,
+        }
+        alcohol += v * p;
+        if x * 100 < alcohol {
+            answer = i as i64 + 1;
+            break;
+        }
+    }
+
+    println!("{}", answer);
 }
