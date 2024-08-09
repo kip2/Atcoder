510fc5178a878a2e11a939a2d0bf16943ec44df3
kip2 <tqa1no4@gmail.com>
2024-08-09 14:54:22 +0900

    Tessoku-book: Clear a17

--- tessoku-book/src/bin/a17.rs
+++ tessoku-book/src/bin/a17.rs
@@ -1,3 +1,53 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a_buf: [usize; n-1],
+        b_buf: [usize; n-2],
+    }
+
+    let mut a = vec![0];
+    let mut b = vec![0, 0];
+
+    a.extend(a_buf);
+    b.extend(b_buf);
+
+    let mut dp = Vec::new();
+    dp.push(0);
+
+    for i in 0..n {
+        if i == 0 {
+            continue;
+        } else if i == 1 {
+            dp.push(a[i]);
+            continue;
+        }
+
+        dp.push(std::cmp::min(a[i] + dp[i - 1], b[i] + dp[i - 2]));
+    }
+
+    let mut p: Vec<i32> = vec![];
+
+    let mut i = n - 1;
+    loop {
+        p.insert(0, i as i32 + 1);
+        if i == 0 {
+            break;
+        }
+        if dp[i - 1] + a[i] == dp[i] {
+            i -= 1;
+        } else {
+            i -= 2;
+        }
+    }
+
+    println!("{}", p.len());
+    println!(
+        "{}",
+        p.iter()
+            .map(|x| x.to_string())
+            .collect::<Vec<String>>()
+            .join(" ")
+    );
 }
