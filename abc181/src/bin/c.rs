153c88e2ed7705c289102d9edda0d6007933003c
kip2 <tqa1no4@gmail.com>
2024-08-09 13:48:06 +0900

    ABC181: Clear c

--- abc181/src/bin/c.rs
+++ abc181/src/bin/c.rs
@@ -1,3 +1,35 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n:usize,
+    }
+
+    let mut x: Vec<i64> = Vec::new();
+    let mut y: Vec<i64> = Vec::new();
+
+    for _ in 0..n {
+        input! {
+            x_buf: i64,
+            y_buf: i64,
+        }
+
+        x.push(x_buf);
+        y.push(y_buf);
+    }
+
+    let mut answer = "No";
+
+    for i in 0..n {
+        for j in i + 1..n {
+            for k in j + 1..n {
+                if (x[j] - x[i]) * (y[k] - y[i]) - (y[j] - y[i]) * (x[k] - x[i]) == 0 {
+                    answer = "Yes";
+                    break;
+                }
+            }
+        }
+    }
+
+    println!("{}", answer);
 }
