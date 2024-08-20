84f16edddebbd62f17ca9dffff735e7cc9315391
kip2 <tqa1no4@gmail.com>
2024-08-20 20:16:57 +0900

    Add comment

--- abc183/src/bin/c.rs
+++ abc183/src/bin/c.rs
@@ -17,41 +17,22 @@ fn main() {
     }
 
     // 経路のカウント用の変数を用意
-    let mut answer = 0;
 
     // 順列を生成する
-    let permutations = (0..n).permutations(n);
 
     // 順列でループ
-    for root in permutations {
-        // 今いる都市、今の時間を初期化
-        let mut now_city = 0;
-        let mut now_time = 0;
+    // 今いる都市、今の時間を初期化
 
-        // 今の都市から次の都市への時間をカウント
-        // 今の年に次の歳をセット
-        now_time += t[now_city][root[0]];
-        now_city = root[0];
+    // 今の都市から次の都市への時間をカウント
+    // 今の年に次の歳をセット
 
-        // 順列の残りをループする
-        // 次の行き先を変数に用意
-        // 今の時間にカウント
-        // 今の都市を更新
-        for i in 1..n {
-            let to_city = root[i];
-            now_time += t[now_city][to_city];
-            now_city = to_city;
-        }
+    // 順列の残りをループする
+    // 次の行き先を変数に用意
+    // 今の時間にカウント
+    // 今の都市を更新
 
-        // 今の時間に、今の都市から出発点に移動する
-        now_time += t[now_city][0];
-
-        // 今の時間がkと一致するならanswerを増やす
-        if now_time == k {
-            answer += 1;
-        }
-    }
+    // 今の時間に、今の都市から出発点に移動する
+    // 今の時間がkと一致するならanswerを増やす
 
     // プリント
-    println!("{}", answer / 2);
 }
