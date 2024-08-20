39a29b5b93a591c7e13a8977d0d9e0cb005213fb
kip2 <tqa1no4@gmail.com>
2024-08-20 20:16:51 +0900

    Tessoku-book: Clear a18 again

--- tessoku-book/src/bin/a18.rs
+++ tessoku-book/src/bin/a18.rs
@@ -1,22 +1,6 @@
 use proconio::input;
 
 fn main() {
-    // inputの取得
-
-    // aに関しては0だけのvecを作成し、受け取ったbufを末尾に追加する
-
-    // dp用の配列を用意する
-
-    // dp[0][0]はtrue
-
-    // 何枚選ぶかをi、選択した結果の数値をjとして配列をループ
-
-    // jがa[i]より低いかどうかで判定
-    // 低いなら、上の行の値がtrueであればしたにそのまま移植する
-
-    // 以上なら、上の業の値がtrueか、あるいはdpの上の行のj-a[i]がtrueなら、trueにする
-
-    // あとはYesかNoかで判定する
     input! {
         n: usize,
         s: usize,
@@ -33,16 +17,15 @@ fn main() {
     for i in 1..=n {
         for j in 0..=s {
             if j < a[i] {
-                if dp[i - 1][j] {
-                    dp[i][j] = true;
-                }
-            } else if j >= a[i] {
+                dp[i][j] = dp[i - 1][j];
+            } else {
                 if dp[i - 1][j] || dp[i - 1][j - a[i]] {
                     dp[i][j] = true;
                 }
             }
         }
     }
+
     if dp[n][s] {
         println!("Yes");
     } else {
