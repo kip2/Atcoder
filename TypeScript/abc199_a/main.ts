3c45d10e29a2e8972dfee131b9e89133a5d80d29
kip2 <tqa1no4@gmail.com>
2025-04-15 07:09:34 +0900

    Refactor solve into separate function

--- TypeScript/abc199_a/main.ts
+++ TypeScript/abc199_a/main.ts
@@ -2,6 +2,11 @@ const input = await new Response(Deno.stdin.readable).text();
 const lines = input.trim().split('\n');
 
 const [a, b, c] = lines[0].split(' ').map(Number);
-console.log(a * a + b * b < c * c ? 'Yes' : 'No');
+
+function solve(a: number, b: number, c: number): string {
+    return a * a + b * b < c * c ? 'Yes' : 'No';
+}
+
+console.log(solve(a, b, c))
 
 export{};
