aa1d8736c54c87d6bec68d25b800dfb97d9bcd9e
kip2 <tqa1no4@gmail.com>
2025-04-15 07:09:38 +0900

    Change template

--- TypeScript/template/main.ts
+++ TypeScript/template/main.ts
@@ -2,6 +2,13 @@ const input = await new Response(Deno.stdin.readable).text();
 const lines = input.trim().split('\n');
 
 const [a, b, c] = lines[0].split(' ').map(Number);
-console.log(a * a + b * b < c * c ? 'Yes' : 'No');
+
+function solve(a: number, b: number, c: number): string {
+    // Todo: implemented me!
+    throw new Error("Not implemented!")
+}
+
+console.log(solve(a, b, c))
 
 export{};
+
