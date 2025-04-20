57d1ceee4d483fc2cc0fb51aa511c18bfceb3c38
kip2 <tqa1no4@gmail.com>
2025-04-20 22:41:18 +0900

    Update template for java

--- Java/template/Main.java
+++ Java/template/Main.java
@@ -2,18 +2,36 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
+        // case: input to int[]
+        // int[] inputs = getLineAsIntArray();
+        // System.out.println("input value: " + Arrays.toString(inputs));
 
-        String result = solve(a, b, c);
-        System.out.println(result);
-
-        sc.close();
+        // case: input to string
+        // String input = getLineAsString();
+        // System.out.println("input value: " + input);
     }
 
-    public static String solve(int a, int b, int c) {
+    public static String getLineAsString() {
+        Scanner sc = new Scanner(System.in);
+        String line = sc.nextLine();
+        sc.close();
+        return line;
+    }
+
+    public static int[] getLineAsIntArray() {
+      Scanner sc = new Scanner(System.in);
+      String line = sc.nextLine();
+      sc.close();
+
+      String[] tokens = line.trim().split("\\s+");
+      int[] nums = new int[tokens.length];
+      for (int i = 0; i < tokens.length; i++) {
+        nums[i] = Integer.parseInt(tokens[i]);
+      }
+      return nums;
+    }
+
+    public static String solve() {
       return null;
     }
 }
