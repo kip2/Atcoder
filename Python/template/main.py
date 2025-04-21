8232afb115bdee41aeea6f2ab8399188331ca021
kip2 <tqa1no4@gmail.com>
2025-04-21 22:19:45 +0900

    Update template for python

--- Python/template/main.py
+++ Python/template/main.py
@@ -1,9 +1,32 @@
 def solve():
-  pass
+    # todo: implemented me!
+    pass
+
+
+def get_input():
+    """
+    Reads a single line of input from standard input and returns it as a string.
+
+    Returns:
+      str: The input line as a string.
+    """
+    return input()
+
+
+def get_int_list():
+    """
+    Reads a line of input, splits it by whitespace, converts each token to an integer,
+    and returns the result as a list of integers.
+
+    Returns:
+      list[int]: A list of integers parsed from the input line.
+    """
+    return list(map(int, input().split))
+
 
 def main():
-    a, b, c = map(int, input().split())
     print(solve())
+
 
 if __name__ == "__main__":
     main()
