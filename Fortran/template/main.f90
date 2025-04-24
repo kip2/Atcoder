ebf5a3cbc2fb12d4880c8f2c10a69a6951e6fcb4
kip2 <tqa1no4@gmail.com>
2025-04-24 21:25:44 +0900

    Add template For fortran

---
+++ Fortran/template/main.f90
@@ -0,0 +1,12 @@
+program main
+  implicit none
+  integer :: a, b, c
+
+  read(*,*) a, b, c
+
+  if (a*a + b*b < c*c) then
+    print '(A)', 'Yes'
+  else
+    print '(A)', 'No'
+  end if
+end program main
