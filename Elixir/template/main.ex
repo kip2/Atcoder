538be9c27baadbb6d60eb429897901567283c3ee
kip2 <tqa1no4@gmail.com>
2025-04-26 21:46:03 +0900

    Update template for eliir

--- Elixir/template/main.ex
+++ Elixir/template/main.ex
@@ -12,4 +12,35 @@ defmodule Main do
   end
 end
 
+defmodule InputHelper do
+  @doc """
+  Get an integer from user input.
+
+  ## Parameters
+  None
+
+  ## Return value
+  - An integer value received from standard input.
+  """
+  def read_integer() do
+    IO.gets("") |> String.trim() |> String.to_integer()
+  end
+
+  @doc """
+  Get a list of integers from standard input.
+
+  ## Parameters
+  None
+
+  ## Return value
+  - A list of integers recieved from standard input.
+  """
+  def read_integer_list() do
+    IO.gets("")
+    |> String.trim()
+    |> String.split()
+    |> Enum.map(&String.to_integer/1)
+  end
+end
+
 Main.main()
