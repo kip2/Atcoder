92d97b1dffaaa5e6d04f45645cd03c66c5730ecd
kip2 <tqa1no4@gmail.com>
2025-04-27 18:48:46 +0900

    Update abc191 b code for elixir

--- Elixir/abc191_b/lib/main.ex
+++ Elixir/abc191_b/lib/main.ex
@@ -1,16 +1,33 @@
 defmodule Main do
   def main do
-    [_ | [x]] = IO.read(:line) |> String.trim() |> String.split(" ")
+    [_ | [x]] = InputHelper.read_to_list()
 
-    IO.read(:line)
-    |> String.trim()
-    |> String.split(" ")
+    InputHelper.read_to_list()
     |> Enum.reject(fn y -> y == x end)
-    |> to_output_format_list()
+    |> OutputHelper.to_output_format_list()
     |> IO.puts()
   end
+end
 
+defmodule OutputHelper do
   def to_output_format_list(list) do
-    list |> Enum.join(" ")
+    list |> Enum.intersperse(" ") |> IO.iodata_to_binary()
+  end
+end
+
+defmodule InputHelper do
+  def read_to_list() do
+    IO.read(:line) |> String.trim() |> String.split()
+  end
+
+  def read_integer() do
+    IO.read(:line) |> String.trim() |> String.to_integer()
+  end
+
+  def read_integer_list() do
+    IO.read(:line)
+    |> String.trim()
+    |> String.split()
+    |> Enum.map(&String.to_integer/1)
   end
 end
