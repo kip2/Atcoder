c587b8b598fdcd19b5b2985da7f1c61a76e1364b
kip2 <tqa1no4@gmail.com>
2025-04-27 14:21:16 +0900

    Update environment

--- Elixir/setup.sh
+++ Elixir/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# set -e
+set -e
 
 if [ $# -ne 1 ]; then
     echo "Usage: ./setup.sh ABC123_a"
@@ -16,13 +16,33 @@ PROBLEM_SUFFIX_LOWER=$(echo "$PROBLEM_SUFFIX" | tr '[:upper:]' '[:lower:]')
 
 oj d "https://atcoder.jp/contests/$CONTEST_ID_LOWER/tasks/${CONTEST_ID_LOWER}_${PROBLEM_SUFFIX_LOWER}"
 
-mkdir -p "$TASK_ID"
-
-# copy clojure template file
-cp ./template/main.ex "$TASK_ID/main.ex"
-
-# copy shell script to execute test 
-cp ./template/run.sh "$TASK_ID/"
+# mkdir -p "$TASK_ID"
+mix new "$TASK_ID" --app main
 
 # move sample data
-mv ./test "$TASK_ID/"
+if [ -d test ]; then
+    mv ./test ./sample
+    mv ./sample "$TASK_ID/"
+else 
+    echo "Error: test directory not found. oj d failed?"
+    exit 1
+fi
+
+# copy template file
+if [ -f ./template/main.ex ]; then
+    cp ./template/main.ex "./$TASK_ID/lib/main.ex"
+else
+    echo "Error: template/main.ex not found"
+    exit 1
+fi
+
+# copy shell script to execute test 
+if [ -f ./template/run.sh ]; then
+    cp ./template/run.sh "$TASK_ID/"
+else
+    echo "Error: template/run.sh not found"
+    exit 1
+fi
+
+# change current directory to project directory
+cd "$TASK_ID"
