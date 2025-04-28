c99f2af823dc675ab09f4036b5ab17d86371c5f0
kip2 <tqa1no4@gmail.com>
2025-04-28 23:05:13 +0900

    Update template for lua

--- Lua/template/main.lua
+++ Lua/template/main.lua
@@ -7,12 +7,25 @@ local function read_inputs()
   return numbers
 end
 
+local function read_number()
+  return tonumber(io.read("*l"))
+end
+
 local function read_a_input()
   local input = io.read("*l")
   return input
 end
 
 local function solve()
+end
+
+-- debug lib
+local function table_to_string(tbl)
+  local result = {}
+  for _, v in ipairs(tbl) do
+    table.insert(result, tostring(v))
+  end
+  return table.concat(result, " ")
 end
 
 local function main()
