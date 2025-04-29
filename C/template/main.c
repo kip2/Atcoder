26f0c28044ef4e0e512fac67ff34af5c6b901a99
kip2 <tqa1no4@gmail.com>
2025-04-29 20:29:57 +0900

    Update template for C

--- C/template/main.c
+++ C/template/main.c
@@ -3,26 +3,42 @@
 
 #define MAX_LEN 100
 
-char *get_input(char *buffer, int size)
-{
-  if (fgets(buffer, size, stdin))
-  {
-    buffer[strcspn(buffer, "\n")] = '\0';
-  }
-  return buffer;
+int get_input_int() {
+    int n;
+    scanf("%d", &n);
+    return n;
 }
 
-void solve()
-{
-  return;
+char *get_input(char *buffer, int size) {
+    if (fgets(buffer, size, stdin)) {
+        buffer[strcspn(buffer, "\n")] = '\0';
+    }
+    return buffer;
 }
 
-int main(void)
-{
-  // sample code
-  // char input[MAX_LEN];
-  // get_input(input, MAX_LEN);
-  // printf("input string: %s\n", input);
-  solve();
-  return 0;
+void get_int_array(int *arr, int max_size) {
+    for (int i = 0; i < max_size; i++) {
+        scanf("%d", &arr[i]);
+    }
+}
+
+void solve() { return; }
+
+int main(void) {
+    // sample code
+    // Get a array of int type.
+    // int X[MAX_LEN];
+    // get_int_array(X, MAX_LEN);
+
+    // sample code2
+    // Get a int value.
+    // int N = get_input_int();
+
+    // sample code 3
+    // char input[MAX_LEN];
+    // get_input(input, MAX_LEN);
+    // printf("input string: %s\n", input);
+
+    solve();
+    return 0;
 }
