c2b1f4eb98ebf583e7383f40d43357e16bb7a5be
kip2 <tqa1no4@gmail.com>
2025-05-01 07:39:11 +0900

    Update template for Rust

--- Rust-oj/template/main.rs
+++ Rust-oj/template/main.rs
@@ -14,3 +14,44 @@ fn main() {
 
     println!("{:?}", nums);
 }
+
+fn solve() -> () {}
+
+fn read_i32_vec() -> Vec<i32> {
+    let stdin = io::stdin();
+    let mut lock = stdin.lock();
+    read_i32_vec_from(&mut lock)
+}
+
+fn read_i32_vec_from<R: BufRead>(reader: &mut R) -> Vec<i32> {
+    let mut line = String::new();
+    reader.read_line(&mut line).unwrap();
+
+    line.split_whitespace()
+        .map(|s| s.parse::<i32>().unwrap())
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::io::Cursor;
+
+    #[test]
+    fn test_read_i32_vec_from() {
+        let input = b"10 20 30\n";
+        let mut cursor = Cursor::new(&input[..]);
+
+        let result = read_i32_vec_from(&mut cursor);
+        assert_eq!(result, vec![10, 20, 30]);
+    }
+
+    #[test]
+    fn test_read_i32_vec_from_with_extra_spaces() {
+        let input = b" 7 8 9 \n";
+        let mut cursor = Cursor::new(&input[..]);
+
+        let result = read_i32_vec_from(&mut cursor);
+        assert_eq!(result, vec![7, 8, 9]);
+    }
+}
