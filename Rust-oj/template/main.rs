f981c7465b25976c3fedf60893306552beb924f5
kip2 <tqa1no4@gmail.com>
2025-05-02 10:47:17 +0900

    Update template for Rust

--- Rust-oj/template/main.rs
+++ Rust-oj/template/main.rs
@@ -17,6 +17,18 @@ fn main() {
 
 fn solve() -> () {}
 
+fn read_string() -> String {
+    let stdin = io::stdin();
+    let mut lock = stdin.lock();
+    read_string_from(&mut lock)
+}
+
+fn read_string_from<R: BufRead>(reader: &mut R) -> String {
+    let mut input = String::new();
+    reader.read_line(&mut input).unwrap();
+    input.trim().to_string()
+}
+
 fn read_i32_vec() -> Vec<i32> {
     let stdin = io::stdin();
     let mut lock = stdin.lock();
@@ -31,11 +43,19 @@ fn read_i32_vec_from<R: BufRead>(reader: &mut R) -> Vec<i32> {
         .map(|s| s.parse::<i32>().unwrap())
         .collect()
 }
-
 #[cfg(test)]
 mod tests {
     use super::*;
     use std::io::Cursor;
+
+    #[test]
+    fn test_read_string() {
+        let input = "abc";
+        let mut cursor = Cursor::new(&input[..]);
+        let actual = read_string_from(&mut cursor);
+
+        assert_eq!(actual, input);
+    }
 
     #[test]
     fn test_read_i32_vec_from() {
