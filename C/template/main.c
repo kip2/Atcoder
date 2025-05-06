94bb5c9acdc31cc90462993dac148b55eb88771c
kip2 <tqa1no4@gmail.com>
2025-05-06 19:47:35 +0900

    Update template for C

--- C/template/main.c
+++ C/template/main.c
@@ -22,6 +22,36 @@ void get_int_array(int *arr, int max_size) {
     }
 }
 
+// To debug print
+void debug_print_int_array(int *arr, int length, char *name) {
+    printf("[DEBUG] %s :", name);
+    for (int i = 0; i < length; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+// To debug print
+void debug_print_int_arr_pointer(int *arr) {
+    printf("[DEBUG] arr first pointer address: %p", (void *)arr);
+}
+
+// To debug print
+void debug_print_int(int n, char *name) { printf("[DEBUG] %s: %d\n", name, n); }
+
+void join_int_array(const int *arr, int length, char *out, const char *sep) {
+    out[0] = '\0';
+
+    for (int i = 0; i < length; i++) {
+        char buffer[32];
+        if (i > 0) {
+            strcat(out, sep);
+        }
+        sprintf(buffer, "%d", arr[i]);
+        strcat(out, buffer);
+    }
+}
+
 void solve() { return; }
 
 int main(void) {
