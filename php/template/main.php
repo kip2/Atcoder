666ee5df878aa975662f83cc2d772e82246153b9
kip2 <tqa1no4@gmail.com>
2025-05-21 22:29:11 +0900

    Update template for php

--- php/template/main.php
+++ php/template/main.php
@@ -9,6 +9,23 @@ function println($s): void
   echo ($s . "\n");
 }
 
+function join_space($arr): string
+{
+  return implode(" ", $arr);
+}
+
+function parse_str_to_int_arr($line): array
+{
+  return array_map('intval', explode(' ', $line));
+}
+
+function get_input_int_arr(): array
+{
+  $line = trim(fgets(STDIN));
+  return array_map('intval', explode(' ', $line));
+}
+
+
 function solve($a, $b, $c)
 {
   "";
