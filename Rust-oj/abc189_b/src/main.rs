547c359af5f142d0f439a8cacc5df615b8c7e652
kip2 <tqa1no4@gmail.com>
2025-06-28 14:41:34 +0900

    Clear abc189 b for Rust

---
+++ Rust-oj/abc189_b/src/main.rs
@@ -0,0 +1,66 @@
+use std::io::{self, BufRead};
+
+fn main() {
+    let stdin = io::stdin();
+    let mut reader = stdin.lock();
+
+    let line = read_i64_vec(&mut reader);
+    let n = line[0];
+    let x = line[1];
+
+    let ans = solve(n, x, &mut reader);
+
+    println!("{}", ans);
+}
+
+fn solve<R: BufRead>(n: i64, x: i64, reader: &mut R) -> i64 {
+    let mut total: i64 = 0;
+
+    for i in 1..=n {
+        let line = read_i64_vec(reader);
+        let v = line[0];
+        let p = line[1];
+
+        let ac = alcohol_content(v, p);
+        total += ac;
+        if total > x * 100 {
+            return i;
+        }
+    }
+    return -1;
+}
+
+fn alcohol_content(alcohol: i64, percent: i64) -> i64 {
+    return alcohol * percent;
+}
+
+#[test]
+fn test_alcohol_content() {
+    let expected = 10;
+
+    let alcohol = 200;
+    let percent = 5;
+    let actual = alcohol_content(alcohol, percent);
+
+    assert_eq!(actual, expected);
+}
+
+fn solve2() -> () {}
+
+fn read_f64_vec<R: BufRead>(reader: &mut R) -> Vec<f64> {
+    let mut line = String::new();
+    reader.read_line(&mut line).unwrap();
+
+    line.split_whitespace()
+        .map(|s| s.parse::<f64>().unwrap())
+        .collect()
+}
+
+fn read_i64_vec<R: BufRead>(reader: &mut R) -> Vec<i64> {
+    let mut line = String::new();
+    reader.read_line(&mut line).unwrap();
+
+    line.split_whitespace()
+        .map(|s| s.parse::<i64>().unwrap())
+        .collect()
+}
