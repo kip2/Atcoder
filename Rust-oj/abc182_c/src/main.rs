752e16bda3851ae6e0f4b9100a31c131e0a50088
kip2 <tqa1no4@gmail.com>
2025-07-01 07:45:08 +0900

    Add abc 182 c

---
+++ Rust-oj/abc182_c/src/main.rs
@@ -0,0 +1,119 @@
+use std::io::{self, BufRead};
+
+fn main() {
+    let stdin = io::stdin();
+    let mut reader = stdin.lock();
+
+    let n = read_i64_single(&mut reader);
+
+    // println!("{:?}", n);
+    // println!("{:?}", a);
+
+    // output: solved value
+    // let value = solve(n, a);
+    // println!("{:?}", value);
+}
+
+fn solve() -> () {}
+
+fn read_string<R: BufRead>(reader: &mut R) -> String {
+    let mut input = String::new();
+    reader.read_line(&mut input).unwrap();
+    input.trim().to_string()
+}
+
+fn read_i64_single<R: BufRead>(reader: &mut R) -> i64 {
+    let mut line = String::new();
+    reader.read_line(&mut line).unwrap();
+    line.trim().parse::<i64>().unwrap()
+}
+
+fn read_i64_vec<R: BufRead>(reader: &mut R) -> Vec<i64> {
+    let mut line = String::new();
+    reader.read_line(&mut line).unwrap();
+
+    line.split_whitespace()
+        .map(|s| s.parse::<i64>().unwrap())
+        .collect()
+}
+
+fn into_string_from_i64_vec(v: Vec<i64>) -> String {
+    v.iter()
+        .map(|x| x.to_string())
+        .collect::<Vec<String>>()
+        .join(" ")
+}
+
+fn read_i32_single<R: BufRead>(reader: &mut R) -> i32 {
+    let mut line = String::new();
+    reader.read_line(&mut line).unwrap();
+    line.trim().parse::<i32>().unwrap()
+}
+
+fn read_i32_vec<R: BufRead>(reader: &mut R) -> Vec<i32> {
+    let mut line = String::new();
+    reader.read_line(&mut line).unwrap();
+
+    line.split_whitespace()
+        .map(|s| s.parse::<i32>().unwrap())
+        .collect()
+}
+
+fn into_string_from_i32_vec(v: Vec<i32>) -> String {
+    v.iter()
+        .map(|x| x.to_string())
+        .collect::<Vec<String>>()
+        .join(" ")
+}
+
+fn read_usize_vec<R: BufRead>(reader: &mut R) -> Vec<usize> {
+    let mut line = String::new();
+    reader.read_line(&mut line).unwrap();
+
+    line.split_whitespace()
+        .map(|s| s.parse::<usize>().unwrap())
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::io::Cursor;
+
+    #[test]
+    fn test_into_sring_from_i32_vec() {
+        let input = vec![1, 2, 3];
+        let expected = "1 2 3".to_string();
+
+        let actual = into_string_from_i32_vec(input);
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_read_string() {
+        let input = "abc";
+        let mut cursor = Cursor::new(&input[..]);
+        let actual = read_string(&mut cursor);
+
+        assert_eq!(actual, input);
+    }
+
+    #[test]
+    fn test_read_i32_vec_from() {
+        let input = b"10 20 30\n";
+        let mut cursor = Cursor::new(&input[..]);
+
+        let result = read_i32_vec(&mut cursor);
+        assert_eq!(result, vec![10, 20, 30]);
+    }
+
+    #[test]
+    fn test_read_i32_vec_from_with_extra_spaces() {
+        let input = b" 7 8 9 \n";
+        let mut cursor = Cursor::new(&input[..]);
+
+        let result = read_i32_vec(&mut cursor);
+        assert_eq!(result, vec![7, 8, 9]);
+    }
+}
