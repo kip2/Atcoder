139d5b6d2892e15ce670b4194e884a4423311d6a
kip2 <tqa1no4@gmail.com>
2025-12-15 00:46:30 +0900

    Clear abc 191 b for D

--- dlang/abc191_b/main.d
+++ dlang/abc191_b/main.d
@@ -6,12 +6,8 @@ import std.algorithm;
 import std.array;
 import std.conv;
 
-string solve(string input) {
-    return input;
-}
-
-string getInput() {
-    return readln().strip();
+int[] solve(int[] arr, int x) {
+    return arr.filter!(a => a != x).array;
 }
 
 int[] getInputOfIntArray() {
@@ -19,7 +15,30 @@ int[] getInputOfIntArray() {
 }
 
 void main() {
-    string result = solve(getInput());
+    int[] line1 = getInputOfIntArray();
+    int[] line2 = getInputOfIntArray();
 
-    writeln(result);
+    int n = line1[0];
+    int x = line1[1];
+
+    int[] result = solve(line2, x);
+    printIntArr(result);
+}
+
+void printIntArr(int[] arr) {
+    writeln(arr.map!(to!string).join(" "));
+}
+
+unittest {
+    int[] arr = [1, 2, 3];
+    int x = 3;
+
+    int[] expected = [1, 2];
+    int[] actual = solve(arr, 3);
+
+    assert(
+        actual == expected,
+        "expected=" ~ expected.to!string ~
+            ", actual=" ~ actual.to!string
+    );
 }
---
+++ dlang/abc191_b/test.fish
@@ -0,0 +1,3 @@
+#!/usr/bin/env fish
+
+dmd -unittest -g -run main.d
