e914dfb6224b03375719bf4edf46d3192ce79c2b
kip2 <tqa1no4@gmail.com>
2026-05-21 21:40:45 +0900

    Update abc065 a for php

--- php/abc065_a/main.php
+++ php/abc065_a/main.php
@@ -1,33 +1,9 @@
 <?php
-function println($s): void
-{
-  echo ($s . "\n");
-}
 
-function solve($x, $a, $b)
-{
-  $result = eat($x, $a, $b);
-
-  println($result);
-}
-
-function eat($x, $a, $b)
-{
-  $daysExpiration = -$a + $b;
-
-  if ($daysExpiration <= 0)
-    return "delicious";
-  else if ($daysExpiration <= $x)
-    return "safe";
-  else
-    return "dangerous";
-}
-
-function main()
-{
-  $line = trim(fgets(STDIN));
-  [$x, $b, $c] = array_map('intval', explode(' ', $line));
-  solve($x, $b, $c);
-}
-
-main();
+[$x, $a, $b] = array_map('intval', explode(' ', trim(fgets(STDIN))));
+$diff = $b - $a;
+echo match (true) {
+  $diff <= 0 => 'delicious',
+  $diff <= $x => 'safe',
+  default => 'dangerous',
+} . PHP_EOL;
