f258ea4e4a363138525bc8d2c4700056b7ece347
kip2 <tqa1no4@gmail.com>
2026-05-26 12:13:41 +0900

    Clear arc065 a for php anothe answer

--- php/arc065_a/main.php
+++ php/arc065_a/main.php
@@ -1,76 +1,22 @@
 <?php
 
-enum State {
-  case START;
-  case DREAM;
-  case ERASE;
-  case ER;
-  case R;
-}
-
 function solve($s): string {
+  $words = ["dream", "dreamer", "erase", "eraser"];
 
-  $length = strlen($s);
-  $i = 0;
-  $state = State::START;
-  $tmp = "";
-  $no = "NO";
-  $yes = "YES";
-
-  while ($i < $length) {
-    switch ($state) {
-      case State::START:
-        $tmp = substr($s, $i, 5);
-        if ($tmp !== "dream" && $tmp !== "erase") {
-          return $no;
-        } else {
-          if ($tmp === "dream") {
-            $state = State::DREAM;
-            $i += 5;
-          } else if ($tmp === "erase") {
-            $state = State::ERASE;
-            $i += 5;
-          }
-        }
+  while ($s != "") {
+    $matched = false;
+    foreach ($words as $word) {
+      if (str_ends_with($s, $word)) {
+        $s = substr($s, 0, strlen($s) - strlen($word));
+        $matched = true;
         break;
-      case State::DREAM:
-        $tmp = substr($s, $i, 5);
-        if ($tmp === "erase") {
-          $state = State::ERASE;
-          $i += 5;
-        } else if ($tmp === "dream") {
-          $state = State::DREAM;
-          $i += 5;
-        } else {
-          $tmp = substr($s, $i, 2);
-          if ($tmp === "er") {
-            $state = State::ER;
-            $i += 2;
-          } else {
-            return $no;
-          }
-        }
-        break;
-      case State::ERASE:
-        $tmp = substr($s, $i, 1);
-        if ($tmp === "r") {
-          $state = State::R;
-          $i += 1;
-        } else {
-          $state = State::START;
-        }
-        break;
-      case State::ER:
-      case State::R:
-        $state = State::START;
-        break;
-      default:
-        return $no;
-        break;
+      }
+    }
+    if (!$matched) {
+      return "NO";
     }
   }
-
-  return $yes;
+  return "YES";
 }
 
 function main() {
